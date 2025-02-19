23ef784956c778c0844106881b9b96294dc8f931
JiWoo-Yang-Tony <147090774+JiWoo-Yang-Tony@users.noreply.github.com>
2025-02-19 17:55:00 -0500

    First commit. Created Client.cs and LoggingServer.py

---
+++ Client/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From Client.cs");
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
